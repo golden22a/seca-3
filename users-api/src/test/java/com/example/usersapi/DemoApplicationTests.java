dc5e9483d6182a971b27d8d61bbfcc01cd2fbafe
golden22a <golden-boy1994@hotmail.fr>
2018-05-21 14:23:15 -0700

    refactured test code

--- users-api/src/test/java/com/example/usersapi/DemoApplicationTests.java
+++ users-api/src/test/java/com/example/usersapi/DemoApplicationTests.java
@@ -5,8 +5,10 @@ import com.example.usersapi.models.User;
 import com.example.usersapi.repositories.UserRepository;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,26 +25,19 @@ import static java.time.zone.ZoneRulesProvider.refresh;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DemoApplicationTests {
 	@Autowired
 	private UserRepository userRepository;
 
+	User firstUser;
+	User secondUser;
 	@Before
 	public void setUp() {
 		userRepository.deleteAll();
-	}
+		firstUser = new User("Ima", "Person");
 
-	@After
-	public void tearDown() {
-		userRepository.deleteAll();
-	}
-
-	@Test
-	public void shouldAllowFullCrudFunctionalityForAUser() throws Exception {
-		User firstUser = new User("Ima", "Person");
-
-		User secondUser = new User("Someone", "Else");
-
+		secondUser = new User("Someone", "Else");
 		Stream.of(firstUser, secondUser)
 				.forEach(user -> {
 					userRepository.save(user);
@@ -53,18 +48,33 @@ public class DemoApplicationTests {
 
 		// Visit the UI in a browser
 		open("http://localhost:4200");
+	}
+
+	@After
+	public void tearDown() {
+		userRepository.deleteAll();
+	}
+
+	@Test
+	public void v1ShouldShowAllUsers() throws Exception {
+
+
 
 		// There should only be two users
 		$$("[data-user-display]").shouldHave(CollectionCondition.size(2));
 		long firstUserId = firstUser.getId();
-		$("#user-" + firstUserId).shouldHave(text(firstUser.getFirstName()+" "+firstUser.getLastName()));
+		$("#user-" + firstUserId).shouldHave(text(firstUser.getFirstName() + " " + firstUser.getLastName()));
 
 
 		long secondUserId = secondUser.getId();
-		$("#user-" + secondUserId).shouldHave(text(secondUser.getFirstName()+" "+secondUser.getLastName()));
+		$("#user-" + secondUserId).shouldHave(text(secondUser.getFirstName() + " " + secondUser.getLastName()));
 
-
+	}
+	@Test
+	public void v2ShouldAddaUser() throws Exception {
 // Visit the new user page
+		long secondUserId = secondUser.getId();
+
 		$("#add-user-form").should(exist);
 
 
@@ -86,25 +96,31 @@ public class DemoApplicationTests {
 		// Check that the data is showing up for the third User
 		Long thirdUserId = secondUserId + 1;
 		$("#user-" + thirdUserId).shouldHave(text("Third User"));
-
+	}
+	@Test
+	public void v3ShouwEditaUser() {
+		long secondUserId = secondUser.getId();
 		// test updating first user
 		$("#modal-" + secondUserId).click();
-		$("#last-name-"+secondUserId).clear();
-		$("#first-name-"+secondUserId).click();
-		$("#last-name-"+secondUserId).sendKeys("firstLastName-edit");
-		$("#first-name-"+secondUserId).sendKeys("firstFirstName-edit");
+		$("#last-name-" + secondUserId).clear();
+		$("#first-name-" + secondUserId).click();
+		$("#last-name-" + secondUserId).sendKeys("firstLastName-edit");
+		$("#first-name-" + secondUserId).sendKeys("firstFirstName-edit");
 //
-		$("#update-"+secondUserId).click();
+		$("#update-" + secondUserId).click();
 		$("#user-" + secondUserId).shouldHave(text("firstFirstName-edit firstLastName-edit"));
 
-
+	}
+	@Test
+	public void v4ShouldDeleteUser(){
 		// Test Deleting the first user
+		long firstUserId = firstUser.getId();
 		$("#user-" + firstUserId).should(exist);
-		$$("[data-user-display]").shouldHave(CollectionCondition.size(3));
+		$$("[data-user-display]").shouldHave(CollectionCondition.size(2));
 
 		$("#delete-" + firstUserId).click();
 		$("#user-" + firstUserId).shouldNot(exist);
-		$$("[data-user-display]").shouldHave(CollectionCondition.size(2));
+		$$("[data-user-display]").shouldHave(CollectionCondition.size(1));
 
 
 	}
