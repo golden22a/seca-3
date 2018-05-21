352878c6c0d35a0980788f03b07bf2909703ec78
golden22a <golden-boy1994@hotmail.fr>
2018-05-21 05:42:46 -0700

    first api call for records

--- ui/src/app/record-module/record/record.component.ts
+++ ui/src/app/record-module/record/record.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-record',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./record.component.css']
 })
 export class RecordComponent implements OnInit {
-
+  @Input('record')
+  record: any;
   constructor() { }
 
   ngOnInit() {
--- ui/src/app/record-module/records/records.component.ts
+++ ui/src/app/record-module/records/records.component.ts
@@ -1,15 +1,19 @@
+import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
-
+import {RecordsService} from '../../records.service';
 @Component({
   selector: 'app-records',
   templateUrl: './records.component.html',
   styleUrls: ['./records.component.css']
 })
 export class RecordsComponent implements OnInit {
-
-  constructor() { }
+  private records;
+  constructor(private htp:Http,private recordService:RecordsService) { }
 
   ngOnInit() {
+    this.recordService
+    .getAllPublicHearings()
+    .subscribe(res=> this.records=res.json())
   }
 
 }
