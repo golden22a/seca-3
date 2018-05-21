03c8b5c45f312ee9a2edac89e0506872b4a015e6
golden22a <golden-boy1994@hotmail.fr>
2018-05-21 15:00:46 -0700

    testing services

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {NgxPaginationModule} from 'ngx-pagination';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
-      ],
+      ],imports:[
+        RouterTestingModule,
+        NgxPaginationModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -18,10 +24,5 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
 });
